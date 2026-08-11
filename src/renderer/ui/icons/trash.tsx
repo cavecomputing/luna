f1865f3d888a42d1044617f99b1e37b372dce6cd
grizzyburr <matt@cavecomputing.io>
2026-08-10 23:14:24 -0400

    Align chat action icons

--- src/renderer/ui/icons/trash.tsx
+++ src/renderer/ui/icons/trash.tsx
@@ -2,11 +2,11 @@ import { svgBase, type IconProps } from './icon-props.js'
 
 export function Trash({ size = 16 }: IconProps): React.JSX.Element {
   return (
-    <svg {...svgBase} viewBox="0 0 20 20" width={size} height={size}>
-      <path d="M4 6h12" />
-      <path d="M8 3h4l1 3H7l1-3Z" />
-      <path d="m6 6 .75 11h6.5L14 6" />
-      <path d="M9 9v5M11 9v5" />
+    <svg {...svgBase} viewBox="0 0 24 24" width={size} height={size} strokeWidth={2}>
+      <path d="M3 6h18" />
+      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+      <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+      <path d="M10 11v6M14 11v6" />
     </svg>
   )
 }
